5e779fc79f7ba617dae335f3630ccc5c7db5bf45
marimo2020 <100014303+marimo2020@users.noreply.github.com>
2025-07-02 02:27:58 +0900

    Add files via upload

---
+++ bookmark/bookmark copy.js
@@ -0,0 +1,25 @@
+//chromeのローカルファイルにつながっている部分をコメントアウトしたバージョンです。
+
+// function loadbookmark() {
+//   const response = fetch("C:\Users\mizuk\AppData\Local\Google\Chrome\User Data\Profile 1\Bookmarks");
+//   return response;
+// }
+// console.log(loadbookmark());
+
+// const fs = require("fs");
+import fs from "fs";
+
+//googlechromeの保存場所からこのディレクトリにコピー
+// const bookmarkfile = "C:/Users/mizuk/AppData/Local/Google/Chrome/User Data/Profile 1/Bookmarks";
+// function filecopy(){
+//     fs.copyFileSync(bookmarkfile,"./bookmark/bookmarks.json");
+// };
+
+// filecopy();
+
+// const bookmarkset = require( "./bookmarks.json" );
+import bookmarkset from "./bookmarks.json" with { type: 'json' };
+const genki = bookmarkset.roots.bookmark_bar.children[3].children
+
+// console.log(genki);
+export const genkiarrey = genki.map((obj) => [obj.name,obj.url]);
